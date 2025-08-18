4be253e2882349ee9433137ec882a9e42f21c33e
sikinski <ulyana.sikinski@gmail.com>
2025-08-18 17:41:09 +0500

    added swagger

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,54 @@
 import { login } from "../controllers/authController.js";
+import { refresh } from "../controllers/refreshController.js";
 
 export default async function authRoutes(fastify) {
-  fastify.post("/auth", login);
+    // fastify.post("/auth", login)
+    fastify.post("/auth", {
+    schema: {
+        body: {
+            type: "object",
+            required: ["username", "password"], // обязательные поля
+            properties: {
+                username: { type: "string", description: "Логин пользователя" },
+                password: { type: "string", description: "Пароль пользователя" }
+            }
+        },
+        response: {
+            200: {
+                type: "object",
+                properties: {
+                    accessToken: { type: "string", description: "JWT токен для доступа" },
+                    refreshToken: { type: "string", description: "JWT токен для обновления" }
+                }
+            },
+            401: { type: "object", properties: { error: { type: "string" } } },
+            403: { type: "object", properties: { error: { type: "string" } } }
+        }
+    },
+    handler: login
+});
+
+    // fastify.post("/refresh", refresh)
+    fastify.post("/refresh", {
+    schema: {
+        body: {
+            type: "object",
+            required: ["refresh_token"],
+            properties: {
+                refresh_token: { type: "string", description: "Refresh токен пользователя" }
+            }
+        },
+        response: {
+            200: {
+                type: "object",
+                properties: {
+                    accessToken: { type: "string" },
+                    refreshToken: { type: "string" }
+                }
+            },
+            401: { type: "object", properties: { error: { type: "string" } } }
+        }
+    },
+    handler: refresh
+});
 }
--- server.js
+++ server.js
@@ -3,10 +3,32 @@
 import Fastify from "fastify";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
 
 dotenv.config();
 
 const fastify = Fastify({ logger: true });
+
+await fastify.register(swagger, {
+  swagger: {
+    info: {
+      title: "Coffee New Day API",
+      description: "API для кофейни New Day",
+      version: "1.0.0",
+    },
+  },
+});
+
+
+await fastify.register(swaggerUI, {
+  routePrefix: "/docs",
+  uiConfig: { docExpansion: "list" }, // none or full
+  staticCSP: true,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+});
 
 // Регистрируем маршрут /auth
 fastify.register(authRoutes);
