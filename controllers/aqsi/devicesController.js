ae7cfda3bc30ed1a69d4dafa37e946f572c95bc1
sikinski <ulyana.sikinski@gmail.com>
2025-10-09 16:42:54 +0500

    added getShopByID

--- controllers/aqsi/devicesController.js
+++ controllers/aqsi/devicesController.js
@@ -16,4 +16,30 @@ export const getDevices = async (request, reply) => {
     } catch (err) {
         return reply.status(500).send("Ошибка при обращении к Aqsi:", err?.response?.data || err.message)
     }
-} 
+}
+
+
+export const getShopByID = async (request, reply) => {
+    const { id } = request.params
+
+    if (!id) {
+        return reply.status(400).send('Параметр ID обязателен')
+    }
+
+    try {
+        const AQSI_KEY = process.env.AQSI_KEY
+        const AQSI_URL = process.env.AQSI_URL
+
+        const response = await axios.get(`${AQSI_URL}/v2/Shops/${id}`, {
+            headers: {
+                'x-client-key': `Application ${AQSI_KEY}`
+            }
+        })
+
+        return reply.status(200).send(response.data)
+
+    } catch (err) {
+        console.error(err);
+        return reply.status(500).send("Ошибка получения данных магазина", err?.response?.data || err.message)
+    }
+}
--- routes/aqsiRoutes.js
+++ routes/aqsiRoutes.js
@@ -1,9 +1,10 @@
-import { getDevices } from "../controllers/aqsi/devicesController.js";
+import { getDevices, getShopByID } from "../controllers/aqsi/devicesController.js";
 import { getReceipts } from "../controllers/aqsi/receiptsController.js";
 
 async function aqsiRoutes(fastify, options) {
     // Devices
     fastify.get('/aqsi/get-devices', getDevices);
+    fastify.get('/aqsi/shops/:id', getShopByID);
 
     // Orders
     fastify.get('/aqsi/get-receipts', getReceipts)
