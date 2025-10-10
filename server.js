4a4b3a3d5d3e384f23dbcab96ba997201c8aa275
sikinski <ulyana.sikinski@gmail.com>
2025-10-10 16:26:30 +0500

    test

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ import './cron/everydayReport.js'
 import './cron/everydayTasks.js'
 
 dotenv.config();
+await prisma.nativeReceipt.deleteMany()
 
 const HOST = process.env.HOST || 'localhost'
 const PORT = process.env.PORT || 3000
