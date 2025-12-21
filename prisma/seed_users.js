d598bf8d68887106441b2c4fef2703dd7dc4d0a3
sikinski <ulyana.sikinski@gmail.com>
2025-12-21 23:57:44 +0500

    fixes

--- prisma/seed_users.js
+++ prisma/seed_users.js
@@ -2,8 +2,6 @@ import prisma from "../config/db.js";
 import bcrypt from "bcrypt";
 
 const users = [
-    { username: "admin", password: "1234", name: 'Админ', role: "admin" },
-    { username: "customer", password: "1111", name: 'Гость', role: "user" },
     { username: "nikita", password: "Monako0073", name: 'Никита Д.', role: "admin" },
     { username: "sergey", password: "Pixel_Creature_Kenia", name: 'Сергей Г.', role: "admin" },
     { username: "ulyana", password: "I7_angel", name: 'Ульяна Ф.', role: "admin" },
